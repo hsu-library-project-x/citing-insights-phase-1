3e73f149dab971fb236a86abb263b38a3f0edaf1
eal376 <42684154+eal376@users.noreply.github.com>
2019-05-29 14:35:30 -0700

    Pseudo Login Page
    Added picture / reformatted login page

--- citing-insights/src/Login.js
+++ citing-insights/src/Login.js
@@ -18,44 +18,38 @@ class Login extends Component{
 		<div id= "login_page" class="animate">
 
 		<Row>
-		<Col xs="7">
-		<div id="image_container" >
+			<Col xs="6">
+				<div id="image_container">
+				{/* <img id="login_image" src="login_img.jpg" alt="library_picture" /> */}
+				</div>
+			</Col>
+			<Col xs="6">
+				<form id="login_form" > 
+					<h1 class="title"> Login </h1>
+					<div class="container" >
+				      {/* div of class container holds the username, password, and 
+				          Login button. Also has a remember me checkbox. Currently remember me is not
+				      	  functional and Login will just take you to our Demo page*/}
+				     
+				        {/* <label for="uname">Username</label> */}
+				        <input type="text" placeholder="Enter Username" name="uname" required/>
 
-		<img id="login_image" src="login_img.jpg" alt="library_picture" />
-		
-		</div>
+				        {/* <label for="psw">Password</label> */}
+				        <input type="password" placeholder="Enter Password" name="psw" required/>
 
-		</Col>
-
-		<Col xs="5">
-		
-		<form id="login_form" > 
-
-
-		<h1 class="title"> Login </h1>
-		<div class="container" >
-	      {/* div of class container holds the username, password, and 
-	          Login button. Also has a remember me checkbox. Currently remember me is not
-	      	  functional and Login will just take you to our Demo page*/}
-	     
-	        {/* <label for="uname">Username</label> */}
-	        <input type="text" placeholder="Enter Username" name="uname" required/>
-
-	        {/* <label for="psw">Password</label> */}
-	        <input type="password" placeholder="Enter Password" name="psw" required/>
-
-	        <br/>
-	         </div>
-	        <div id="bottom_of_login">
-	        <button type="submit">Login</button>
-	        <label>
-	          Remember Me
-	          <input type="checkbox" checked="checked" name="remember"/>
-	        </label>
-	        </div>
-	     
-	      </form>
-	      </Col>
+				        <br/>
+				    </div>
+				        
+				    <div id="bottom_of_login">
+				        <button type="submit">Login</button>
+				        <label>
+				          Remember Me
+				          <input type="checkbox" checked="checked" name="remember"/>
+				        </label>
+				    </div>
+			     
+			     </form>
+		     </Col>
 	    </Row>
 	    </div>
 
