104f18071278941273c9fdfc7d535f58c40e7230
Mitchell <mkwsoftware@gmail.com>
2019-06-05 11:50:13 -0700

    correct port listening

--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var cars = require('./routes/carRoutes');
 
 var app = express();
 
@@ -14,12 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/cars', cars);
 
 module.exports = app;
 
 
-app.listen(process.env.PORT || 5000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
