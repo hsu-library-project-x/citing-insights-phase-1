b527d7a06fce7a63608ca7ba34aca5b7f7a74cf2
Benmoony <benmiller92@gmail.com>
2019-06-05 10:13:00 -0700

    Added Collapsable menu bar

--- citing-insights/src/Main.js
+++ citing-insights/src/Main.js
@@ -6,18 +6,30 @@ import './App.css';
 import { Switch, Route, HashRouter, Link } from "react-router-dom";
 import Classes from "./Classes";
 
+function CollapseMain(props){
+	let sidebar = document.getElementById("SideBar");
+	if(sidebar.classList.contains("collapse-main")){
+		sidebar.classList.add("expand-main");
+		sidebar.classList.remove("collapse-main");
+	}
+	else{
+		sidebar.classList.remove("expand-main");
+		sidebar.classList.add('collapse-main');
+	}
+	
+}
+
 // Class to render our homepage
 class Main extends Component{
-
 	render(){
 		return(
 			<div id="MainContainer">
 				
-				<h1 class="head-1 head-2">Main Menu<div class="mainCollapse"><div class="miniCircle"><p>-</p></div></div></h1>
+				<h1 class="head-1 head-2">Main Menu<div class="mainCollapse"><div class="miniCircle" onClick={CollapseMain}></div></div></h1>
 					<HashRouter>
-						<div class="sidebar">
+						<div class="sidebar" id="SideBar">
 							<div class="button-container">
-								<Link to="/main/account">
+								<Link to="/main/classes">
 									<button class="menuButton">
 										<p>One</p>
 										<p>Button Text Goes Here: Testing Overflow blabalb alb lablablabl abla blabla blab lablabl</p>
@@ -25,13 +37,13 @@ class Main extends Component{
 								</Link>
 							</div>
 							<div class="button-container">
-								<Link to="/main/classes"><button class="menuButton">Two</button></Link>
+								<Link to="/main/assignment"><button class="menuButton">Two</button></Link>
 							</div>
 							<div class="button-container">
-								<Link to=""><button class="menuButton">Three</button></Link>
+								<Link to="/main/continue"><button class="menuButton">Three</button></Link>
 							</div>
 							<div class="button-container">
-								<Link to=""><button class="menuButton">Four</button></Link>
+								<Link to="/main/download"><button class="menuButton">Four</button></Link>
 							</div>
 						</div>
 					</HashRouter> 
