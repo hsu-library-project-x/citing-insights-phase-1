8f3e3f8c8da9d6a7ba0dc81f8d159caf676a9a58
Mitchell <mkwsoftware@gmail.com>
2019-06-05 10:14:53 -0700

    added listening on port

--- server/app.js
+++ server/app.js
@@ -18,3 +18,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+
+app.listen(process.env.PORT || 5000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
