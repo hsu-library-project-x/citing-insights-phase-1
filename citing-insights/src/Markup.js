101cd642062fb3f8dff1848b976b8fa7f09577de
eal376 <42684154+eal376@users.noreply.github.com>
2019-06-12 12:10:25 -0700

    added comments
    commented markup.js

--- citing-insights/src/Markup.js
+++ citing-insights/src/Markup.js
@@ -1,25 +1,35 @@
+//importing react and reactstrap
 import React, { Component } from 'react';
 import {Input, Button, Form} from 'reactstrap';
+
+//import our css
 import './css/Markup.css';
 
+//this function returns user highlighted text
 function getSelectionText() {
     var text = "";
     if (window.getSelection) {
         text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control"){
+    } 
+    else if (document.selection && document.selection.type != "Control"){
         text = document.selection.createRange().text;
-    }else{
+    }
+    else{
             alert('no')
     }
     return text;
 }
 
+//function to test functionality ....to be removed later
 function testGetTextArea(props) {
 	let test = document.getElementById("highlightText").value;
 	alert(test);
 }
 
+// Markup is a class that creates a component 
+//		This class defines the html that makes up the markup component used in analyze page
 class Markup extends Component {
+	//initalized values
 	constructor(props){
 		super(props);
 		this.state = {
@@ -28,6 +38,7 @@ class Markup extends Component {
 		this.setHighlightedText = this.setHighlightedText.bind(this);
 	}
 
+	//function that changes the state of this to be what was highlighted
 	setHighlightedText(){
 		let highlight = getSelectionText();
 		this.setState({
@@ -35,15 +46,19 @@ class Markup extends Component {
 		})
 	}
 
+	//rendering html 
   	render() {	
 	    return(
-	    	
 	    	<div class="markup-container">
+	    		{/* p tag important for now  */}
 	    		<p>Highlight text to begin marking in-text citations</p>
 	    		<p>This component allows faculty and staff to select and mark intext citations</p>
+		    	{/*user clicks this button to change the state of what was highlighted */}
 		    	<Button onMouseDown={this.setHighlightedText}>Get Highlighted Text</Button>
 		    	<Form>
+		    		{/* where highlighed text goes*/}
 		    		<textarea id="highlightText" value={this.state.curHighlight} ref="highlightArea"></textarea>
+		    		{/* Sources so we can pair intext citation with source.....will be dynamically populated with API*/}
 		    		<Input type="select" name="citation" id="citationSelect">
 						<option>Source 1</option>
 						<option>Source 2</option>
@@ -51,6 +66,7 @@ class Markup extends Component {
 						<option>Source 4</option>
 						<option>Source 5</option>
 					</Input>
+					{/* Button to submit In-Text Citation */}
 		    		<Button onClick={testGetTextArea}>Submit In-Text Citation</Button>
 		    	</Form>
 			</div>
