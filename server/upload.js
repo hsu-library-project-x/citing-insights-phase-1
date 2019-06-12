dcdcd5539157f3912e05a149d69afcd16afebc3a
Mitchell <mkwsoftware@gmail.com>
2019-06-12 13:54:10 -0700

    multiple file upload works. no error messages tho

--- server/upload.js
+++ server/upload.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const shell = require('shelljs')
 
+var Chance = require('chance');
+var chance = new Chance();
+
+
 var paperModel = require('./models/paperModel.js');
 
 
@@ -26,15 +30,24 @@ module.exports = function upload(req, res) {
       console.log('received');
       console.log(file.path);
 
+      // this length be increased if there are collisions
+      var file_name = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz', length: 10});
+
       //Ghostscript strips pdf into raw text
-      shell.exec('gs -sDEVICE=txtwrite -o output.txt ' + file.path);
+      var txt_path = './tmp/txt/' + file_name + '.txt'
+      shell.exec('gs -sDEVICE=txtwrite -o ' + txt_path + ' ' + file.path);
+      
 
       //the replace functions just get rid of carriage returns
-      var raw_text = { "body": fs.readFileSync('output.txt').toString().replace(/\r+/g, "").replace(/\n+/g, ""), "title" : null, "name": null };  
+      var raw_text = { "body": fs.readFileSync(txt_path).toString().replace(/\r+/g, "").replace(/\n+/g, ""), "title" : null, "name": null };  
 
       var paper = new paperModel(raw_text);
 
       paper.save(function (err, paper) {
+
+        // we actually want to set a variable to see whether or not things happenned successfully
+
+        /*
         if (err) {
           return res.status(500).json({
             message: 'Error when creating paper',
@@ -42,11 +55,14 @@ module.exports = function upload(req, res) {
           });
         }
         return res.status(201).json(paper);
+        */
       });
-
     })
     .on('end', () => {
+      //we want to check a bool set in paper.save to see if we cool
+      res.send("we cool");
       console.log('ending');
+      //shell.exec('rm ' + txt_path);
     })
   form.parse(req);
 }
