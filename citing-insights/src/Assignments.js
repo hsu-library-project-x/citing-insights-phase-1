b1fc1c6487b0c93204eb6d1b9518d01d621df873
Benmoony <benmiller92@gmail.com>
2019-06-12 14:16:03 -0700

    Implemented Intial Dropzone
    Intial Dropzone now implemented within Assignment.js, need to add
    functionality that will add class data on upload

--- citing-insights/src/Assignments.js
+++ citing-insights/src/Assignments.js
@@ -4,16 +4,117 @@ import { Row, Col } from 'reactstrap';
 import './css/App.css';
 import './css/Assignment.css';
 
+import Dropzone from './Dropzone';
+import Progress from './Progress';
 
 // Class to render our homepage
 class Assignment extends Component{
-	
-	
-	render(){
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	        files: [],
+	        uploading: false,
+	        uploadProgress: {},
+	        successfullUploaded: false
+	    };
+
+	    this.onFilesAdded = this.onFilesAdded.bind(this);
+	    this.uploadFiles = this.uploadFiles.bind(this);
+	    this.sendRequest = this.sendRequest.bind(this);
+	    this.renderActions = this.renderActions.bind(this);
+    }
+
+    onFilesAdded(files){
+		this.setState(prevState => ({
+			files: prevState.files.concat(files)
+		}));
+	}
+
+	renderProgress(file) {
+		  const uploadProgress = this.state.uploadProgress[file.name];
+		  if (this.state.uploading || this.state.successfullUploaded) {
+		    return (
+		        <div className="ProgressWrapper">
+			        <Progress progress={uploadProgress ? uploadProgress.percentage : 0} />
+			        <img
+			            className="CheckIcon"
+			            alt="done"
+			            src="baseline-check_circle_outline-24px.svg"
+			            style={{ opacity: uploadProgress && uploadProgress.state === "done" ? 0.5 : 0}}
+			        />
+		        </div>
+		    );
+		}
+	}
+
+	renderActions() {
+		if (this.state.successfullUploaded) {
+		    return (
+		        <button onClick={() => this.setState({ files: [], successfullUploaded: false })}>Clear</button>
+		    );
+		} else {
+		    return (
+		        <button disabled={this.state.files.length < 0 || this.state.uploading} onClick={this.uploadFiles}>Upload</button>
+		    );
+		}
+	}
+
+	async uploadFiles() {
+	    this.setState({ uploadProgress: {}, uploading: true });
+	    const promises = [];
+	    this.state.files.forEach(file => {
+	    	promises.push(this.sendRequest(file));
+	    });
+	  	try {
+	    	await Promise.all(promises);
+	    	this.setState({ successfullUploaded: true, uploading: false });
+	  	} catch (e) {
+	    	// Not Production ready! Do some error handling here instead...
+	    	this.setState({ successfullUploaded: true, uploading: false });
+	  	}
+	}
+
+	sendRequest(file) {
+	 return new Promise((resolve, reject) => {
+		const req = new XMLHttpRequest();
+
+		req.upload.addEventListener("progress", event => {
+		    if (event.lengthComputable) {
+			    const copy = { ...this.state.uploadProgress };
+			    copy[file.name] = {
+			    	state: "pending",
+			    	percentage: (event.loaded / event.total) * 100
+			    };
+			    this.setState({ uploadProgress: copy });
+		    }
+		});
+		   
+		req.upload.addEventListener("load", event => {
+		    const copy = { ...this.state.uploadProgress };
+		    copy[file.name] = { state: "done", percentage: 100 };
+		    this.setState({ uploadProgress: copy });
+		    resolve(req.response);
+		});
+		   
+		req.upload.addEventListener("error", event => {
+		    const copy = { ...this.state.uploadProgress };
+		    copy[file.name] = { state: "error", percentage: 0 };
+		    this.setState({ uploadProgress: copy });
+		    reject(req.response);
+		});
+
+		const formData = new FormData();
+		formData.append("file", file, file.name);
+
+		req.open("POST", "http://localhost:5000/upload");
+		req.send(formData);
+	 });
+	}
+	render(){	
 		return(
 			/* So far our homepage is just a h1 tag with text */
 			<div class="classes-container assign-container" >
-				<h1>Upload Assignment</h1>
+				<h1>Upload Assignments</h1>
 				<Row>
 					<Col xs="2">
 					</Col>
@@ -30,13 +131,25 @@ class Assignment extends Component{
 							        <option value="5">Class: 5</option>
 							    </Input>
 							</FormGroup>
-							<FormGroup class="uploadWrapper">
-								<Label for="UploadDoc">Upload File</Label>
-          						<Input type="file" name="paper" id="UploadDoc" required/>
-							</FormGroup>
-							<FormGroup>
-								<Button>Submit</Button>
-							</FormGroup>
+							<div className="Upload">
+        						<span className="Title">Upload Files</span>
+        						<div className="Content">
+          							<div>
+            							<Dropzone onFilesAdded={this.onFilesAdded} disabled={this.state.uploading || this.state.successfullUploaded}/>
+          							</div>
+          							<div className="Files">
+            							{this.state.files.map(file => {
+							              	return (
+								                <div key={file.name} className="Row">
+								                  	<span className="Filename">{file.name}</span>
+								                  	{this.renderProgress(file)}
+								                </div>
+							              	);
+							            })}
+							        </div>
+							    </div>
+							    <div className="Actions">{this.renderActions()}</div>
+							</div>
 						</Form>
 					</Col>
 					<Col xs="2">
