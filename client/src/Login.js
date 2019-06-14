4c5d966b6e608cce8996c57ba5be7f9e4514d129
eal376 <42684154+eal376@users.noreply.github.com>
2019-06-14 11:58:54 -0700

    Changed Login To use Toggles

--- client/src/Login.js
+++ client/src/Login.js
@@ -12,22 +12,72 @@ import './css/login.css';
 //import picture
 import login from './images/UniversityCenterXLg.jpg';
 
-
-function createAccount(props){
-	document.getElementById("login_page").style.display="none";
-	document.getElementById("signup").style.display="block";				
-}
-
-function goBackLogin(props){
-	document.getElementById("login_page").style.display="block";
-	document.getElementById("signup").style.display="none";
-}
-
 function forgotInfo(props){
 	window.location.href="#/passrecov";
 }
 
+const LoginForm = () => (
+	<Form> 
+		<h1> Welcome Back! </h1>
+		<FormGroup class="container">
+	      {/* div of class container holds the username, password, and 
+	          Login button. Also has a remember me checkbox. Currently remember me is not
+	      	  functional and Login will just take you to our Demo page*/}
+	        {/* <label for="uname">Username</label> */}
+	        <input type="text" placeholder="Enter Username" class="uname" required/><br />
+	        {/* <label for="psw">Password</label> */}
+	        <input type="password" placeholder="Enter Password" class="psw" required/>
+	        <br/>
+	        <button class="back" onClick={forgotInfo}>Forgot</button>
+	        <button class="continue">Login</button>
+	        <br /> <br />
+	        {/*href placeholder for now */}
+	        
+	    </FormGroup>
+     </Form>
+	)
+
+const SignupForm = () => (
+	<Form>
+		<h1> Create an Account </h1>
+		<FormGroup class="container">
+			<input type="email" class="email" placeholder="Enter Email" /> <br />
+			<input type="text" class="uname" placeholder="Enter Username" /><br />
+			<input type="password" class="psw" placeholder="Enter Password" /><br />
+			<input type="password" class="psw" placeholder="Confirm Password" />
+			<br />
+		</FormGroup>
+	</Form>
+)
+
 class Login extends Component{
+
+	constructor(props)
+	{
+		super(props);
+		this.state= {
+			haveAccount: true
+		}
+		this.renderActions=this.renderActions.bind(this);
+		this.ToggleLogin= this.ToggleLogin.bind(this);
+	}
+
+	renderActions(){
+		if(!this.state.haveAccount){
+			return (<SignupForm />);
+		}
+		else{
+			return (<LoginForm />);
+		}
+	}
+
+	ToggleLogin()
+	{
+		this.setState({
+		haveAccount : !this.state.haveAccount
+		})
+	}
+
 	render(){
 		return(
 		<div class="container">
@@ -40,49 +90,8 @@ class Login extends Component{
 					</Col>
 					<Col xs="6">
 						<div class="beside_picture">					
-							<Form> 
-								<h1> Welcome Back! </h1>
-								<FormGroup class="container">
-							      {/* div of class container holds the username, password, and 
-							          Login button. Also has a remember me checkbox. Currently remember me is not
-							      	  functional and Login will just take you to our Demo page*/}
-							        {/* <label for="uname">Username</label> */}
-							        <input type="text" placeholder="Enter Username" class="uname" required/><br />
-							        {/* <label for="psw">Password</label> */}
-							        <input type="password" placeholder="Enter Password" class="psw" required/>
-							        <br/>
-							        <button class="back" onClick={forgotInfo}>Forgot</button>
-							        <button class="continue">Login</button>
-							        <br /> <br />
-							        {/*href placeholder for now */}
-							        <button class="acnt_stuff" color="primary" onClick={createAccount}>Sign Up</button>
-							    </FormGroup>
-						     </Form>
-						</div>
-					</Col>
-				</Row>
-			</div>
-			<div id="signup">
-				<Row>
-					<Col xs="5">
-						<div id="image_container2">
-							<img id="createImage" alt="" src={login} />
-						</div>
-					</Col>
-					<Col xs="7">
-						<div class="beside_picture">
-							<Form>
-							<h1> Create an Account </h1>
-								<FormGroup class="container">
-									<input type="email" class="email" placeholder="Enter Email" /> <br />
-									<input type="text" class="uname" placeholder="Enter Username" /><br />
-									<input type="password" class="psw" placeholder="Enter Password" /><br />
-									<input type="password" class="psw" placeholder="Confirm Password" />
-									<br />
-									<button class="back" color="danger" onClick={goBackLogin}> Cancel </button>
-									<button class="continue" color="success"> Submit </button>
-								</FormGroup>
-							</Form>
+							{this.renderActions()}
+							{(this.state.haveAccount) ? <button onClick={this.ToggleLogin}> Sign Up </button>: <div ><button > Make me an Account</button> <button onClick={this.ToggleLogin}> Go Back</button> </div>}
 						</div>
 					</Col>
 				</Row>
