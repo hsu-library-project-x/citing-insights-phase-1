9c48448eaffd18ee830a0f9dc6d84acf49e29f96
Benmoony <benmiller92@gmail.com>
2019-06-17 14:18:21 -0700

    Finished baseline Rubric functionality
    Rubric uses state based architecture to prevent user from uploading
    until rubric is filled and saved

--- client/src/RubricEditor.jsx
+++ client/src/RubricEditor.jsx
@@ -35,7 +35,8 @@ class RubricEditor extends Component{
 	      	rubricData: [],
 	      	isEditing: false,
 	      	isSelecting: true,
-	      	needsSaving: true
+	      	needsSaving: true,
+	      	uploading: false
 	    }
 	    uniqueId.enableUniqueIds(this);
 	    this.buildEditor = this.buildEditor.bind(this);
@@ -44,6 +45,23 @@ class RubricEditor extends Component{
 	    this.reset = this.reset.bind(this);
 	    this.sendRequest = this.sendRequest.bind(this);
 	    this.saveCard = this.saveCard.bind(this);
+	    this.onInput = this.onInput.bind(this);
+	    this.fillButtonText = this.fillButtonText.bind(this);
+	}
+
+	fillButtonText(){
+		if(this.state.needsSaving){
+			return("Please Save Data before Building");
+		}
+		else{
+			return("Build Rubric");
+		}
+	}
+
+	onInput(){
+		this.setState({
+			needsSaving: true
+		});
 	}
 
 	renderActions(){
@@ -54,13 +72,13 @@ class RubricEditor extends Component{
 			for(let i = 0; i < loop; i++){
 				idArray[i] = this.nextUniqueId();
 				this.state.rubricArray.push(
-					<div className={`cardContainer ${this.state.needsSaving ? "warnHighlight" : "safeHighlight"}`}>
+					<div className={`cardContainer `}>
 						<Card>
 							<CardBody>
 								<CardTitle for={"Title-"+idArray[i]}>Rubric Item Title</CardTitle>
-								<Input type="text" id={"Title-"+idArray[i]} class="rubricTitles"/>
+								<Input onInput={this.onInput} type="text" id={"Title-"+idArray[i]} class="rubricTitles"/>
 								<CardText for={"Text-"+idArray[i]}>Rubric Descriptions</CardText>
-								<Input type="textarea" id={"Text-"+idArray[i]} class="rubricDescriptions"/>
+								<Input onInput={this.onInput} type="textarea" id={"Text-"+idArray[i]} class="rubricDescriptions"/>
 							</CardBody>
 						</Card>
 					</div>
@@ -85,12 +103,19 @@ class RubricEditor extends Component{
 			rubricSize: 0,
 			rubricArray: [],
 			rubricData: [],
+			needsSaving: true
 		});
 	}
 
 	//Saves the Current Information in the Card
 	saveCard(idArray){	
 		//grabs information in the card and stores it in the Rubric Array state
+		let rubricTitle = document.getElementById("rubricTitle");
+		if(rubricTitle.value === ""){
+			alert("Please enter a title for your rubric");
+			return;
+		}
+
 		for(let i = 0; i < idArray.length; i++){
 			let id = idArray[i];
 			let cardNum = i;
@@ -109,15 +134,15 @@ class RubricEditor extends Component{
 				let dummyArray = this.state.rubricData;
 				if(dummyArray.length === 0 || dummyArray[cardNum] === null){
 					const cardData = '{ "card'+ cardNum +'" : [' +
-						'{ "rubricTitle": '+ title +' },' +
-						'{ "rubricText": '+ text +' }' + 
+						'{ "cardTitle": '+ title +' },' +
+						'{ "cardText": '+ text +' }' + 
 					']}';
 					this.state.rubricData.push(cardData);
 				}
 				else{
 					const cardData = '{ "card'+ cardNum +'" : [' +
-						'{ "rubricTitle": '+ title +' },' +
-						'{ "rubricText": '+ text +' }' + 
+						'{ "cardTitle": '+ title +' },' +
+						'{ "cardText": '+ text +' }' + 
 					']}';
 					this.state.rubricData[cardNum] = cardData;
 				}
@@ -131,30 +156,50 @@ class RubricEditor extends Component{
 
 	//sends data to the server
 	buildRubric(){
+		let rubricTitle = document.getElementById("rubricTitle");
 		//Grab all the information from the Rubric Data Array
-
 		//Compile this information into a JSON File
+		let jsonData = JSON.stringify(this.state.rubricData);
+		this.setState.uploading = true;
+		const promise = [];
 		//Start the HTTP Requests with promises
+		promise.push(this.sendRequest(rubricTitle.value, jsonData));
+		try{
+			this.setState({
+				uploading: false
+			});
+		}
+		catch (e){
+			//errorcatching here
+			this.setState({
+				uploading: false			
+			});
+		}
 	}
 
-	sendRequest(){
-
+	sendRequest(rubricTitle, data){
+		return new Promise((resolve, reject) => {
+			const req = new XMLHttpRequest();
+			const formData = new FormData();
+			formData.append(rubricTitle, data);
+			req.open("POST", "http://localhost:5000/addRubric");
+			req.send(formData);
+		 });
 	}
 
 	render(){
 		return(
 			<div className={`rubricEdit-container`}>
 				{(!this.state.isEditing) ? <Editor /> : 
-					<div id="cardStorage">
-						<Label for="rubricTitle">Rubric Title</Label>
+					<div className={`${this.state.needsSaving ? "warnHighlight" : "safeHighlight"}`} id="cardStorage">
 						<Input type="text" id="rubricTitle" placeholder="Type Rubric Title Here"/>
 						{this.renderActions()}
 					</div>
 				}
 				{(!this.state.isEditing) ? <Button id="rubEditButton" onClick={this.buildEditor}>Submit</Button> : 
 					<div class="rubricButtonContainer">
-						<Button id="rubBuildButton" onClick={this.buildRubric}>Build Rubric</Button>
-						<Button id="backSelect" onClick={this.reset}>Back</Button>
+						<button disabled={this.state.needsSaving} id="rubBuildButton" onClick={this.buildRubric}>{this.fillButtonText()}</button>
+						<button id="backSelect" onClick={this.reset}>Back</button>
 					</div>
 				}
 			</div>
