8aa7ee6b21ee7c84efb94cbf9eb4eee5f3a8d4c6
Kyle Smith <40129664+smittythehippy@users.noreply.github.com>
2019-10-08 10:14:17 -0700

    Delete config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'googleAuth': {
-        'clientID': '203897182687-jre1rt3vp7lgqi1nvpo3p2gp8jgpreno.apps.googleusercontent.com',
-        'clientSecret': 'cZgZQVlt00JCytjc3jgDyZ2T',
-        //'callbackURL': 'http://localhost:3000/user/login/callback'
-    }
-};
