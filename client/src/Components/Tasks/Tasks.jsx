5358970ed79d2065bd2ff09adeddcf442725297f
eal376 <42684154+eal376@users.noreply.github.com>
2019-12-05 14:13:47 -0800

    switching to material-ui

--- client/src/Components/Tasks/Tasks.jsx
+++ client/src/Components/Tasks/Tasks.jsx
@@ -7,113 +7,68 @@ import RubricEditor from "../Rubric/RubricEditor.jsx";
 import AnalyzeSubMenu from "../Analyze/AnalyzeSubMenu.jsx";
 import Results from '../Overview/Results.jsx';
 
-import {Grid} from "@material-ui/core";
-
 import addClass from './class.svg';
 import addAssignment from './assignment.svg';
 import Continue from './continue.svg';
 import download from './download.svg';
 import rubric from './rubric.svg';
 
-// // Collapses the big drop down of all sub components
-// function CollapseMain() {
-// 	let sidebar = document.getElementById("SideBar");
-// 	let buttons = document.getElementsByClassName("menuButton");
-// 	if (sidebar.classList.contains("collapse-main")) {
-// 		sidebar.classList.remove("collapse-main");
-// 		sidebar.classList.add("expand-main");
-// 		for (let i = 0; i < buttons.length; i++) {
-// 			buttons[i].classList.remove("opacityHideAnimation");
-// 			buttons[i].classList.add("opacityAnimation");
-// 		}
-//
-// 	}
-// 	else {
-// 		for (let i = 0; i < buttons.length; i++) {
-// 			buttons[i].classList.remove("opacityAnimation");
-// 			buttons[i].classList.add("opacityHideAnimation");
-// 		}
-// 		sidebar.classList.remove("expand-main");
-// 		sidebar.classList.add('collapse-main');
-// 	}
-// }
-//----------------------------------------------
-
-
 // Class to render our homepage
 class Tasks extends Component {
-
-	// This changes the page title to correspond to what was clicked
-	changeName(evt) {
-		const id = evt.target.id;
-		let head = document.getElementById("headTitle");
-		head.innerHTML = id;
-	}
-
 	render() {
 		return (
 			<div id="MainContainer">
-				{/*<div className="mainCollapse">             DOESN'T WORK ANYMORE*/}
-				{/*	<button className="miniCircle" onClick={CollapseMain} />*/}
-				{/*</div>*/}
 				<div className="sidebar" id="SideBar">
 					<Grid container spacing={3}>
 						<Grid item xs={12}>
 							<h1 className="head-1 head-2" id="headTitle">Tasks</h1>
 						</Grid>
+						<Grid item xs={1} ></Grid>
 						<Grid item xs={2}>
-							<div className="button-container">
+
 								<Link to='/tasks/courses' >
-									<button className="menuButton" onClick={this.changeName}>
 										<img  alt="classesIcon" id="Class" src={addClass} />
 										Manage Courses
-										</button>
-								</Link>
-							</div>
-						</Grid>
 
+								</Link>
+
+						</Grid>
 						<Grid item xs={2}>
-							<div className="button-container">
+
 								<Link to="/tasks/assignments">
-									<button className="menuButton" onClick={this.changeName}>
+
 										<img  alt="assignmentIcon" id="Upload" src={addAssignment} />
 										Upload Papers
-									</button>
+
 								</Link>
-							</div>
+
 						</Grid>
 
 						<Grid item xs={2}>
-							<div className="button-container">
-								<Link to="/tasks/analyzemenu">
-									<button className="menuButton" onClick={this.changeName}>
+								<Link  to="/tasks/analyzemenu">
+
 										<img  alt="analyzeIcon" id="Analyze" src={Continue} />
 										Analyze
-									</button>
+
 								</Link>
-							</div>
 						</Grid>
 
 						<Grid item xs={2}>
-							<div className="button-container">
-								<Link to="/tasks/overview">
-									<button className="menuButton" onClick={this.changeName}>
+								<Link  to="/tasks/overview">
+
 										<img  alt="downloadIcon" id="Overview" src={download} />
 										Overview
-									</button>
+
 								</Link>
-							</div>
 						</Grid>
 
 						<Grid item xs={2}>
-							<div className="button-container">
-								<Link to="/tasks/rubriceditor">
-									<button className="menuButton" onClick={this.changeName}>
+								<Link  to="/tasks/rubriceditor">
+
 										<img  alt="rubricIcon" id="Rubric" src={rubric} />
 										Edit Rubrics
-									</button>
+
 								</Link>
-							</div>
 						</Grid>
 					</Grid>
 
