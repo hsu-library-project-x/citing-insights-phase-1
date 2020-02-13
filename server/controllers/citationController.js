a1270e06f0f955ffd1ed4113f0a3eb68ec0a4746
smittythehippy <fenderpearl72@gmail.com>
2020-02-13 13:58:47 -0800

    Removed bug allowing for duplicate assessments

--- server/controllers/citationController.js
+++ server/controllers/citationController.js
@@ -70,81 +70,31 @@ module.exports = {
     });
   },
 
-  /**
-   * citationController.create()
-   */
-  create: function (req, res) {
-    var citation = new citationModel({
-      author: req.body.author,
-      date: req.body.date,
-      editor: req.body.editor,
-      edition: req.body.edition,
-      volume: req.body.volume,
-      pages: req.body.pages,
-      type: req.body.type,
-      title: req.body.title,
-      annotation: req.body.annotation,
-      doi: req.body.doi,
-      paper_id: req.body.paper_id
-
-    });
-
-    citation.save(function (err, citation) {
-      if (err) {
-        return res.status(500).json({
-          message: 'Error when creating citation',
-          error: err
-        });
-      }
-      return res.status(201).json(citation);
-    });
-  },
-
-  /**
-   * citationController.update()
-   */
-  update: function (req, res) {
+  //Actually update_assessment
+  remove_assessment: function (req, res) {
     var id = req.params.id;
-    citationModel.findOne({ _id: id }, function (err, citation) {
-      if (err) {
-        return res.status(500).json({
-          message: 'Error when getting citation',
-          error: err
-        });
-      }
-      if (!citation) {
-        return res.status(404).json({
-          message: 'No such citation'
-        });
-      }
 
-      citation.author = req.body.author ? req.body.author : citation.author;
-      citation.date = req.body.date ? req.body.date : citation.date;
-      citation.editor = req.body.editor ? req.body.editor : citation.editor;
-      citation.edition = req.body.edition ? req.body.edition : citation.edition;
-      citation.volume = req.body.volume ? req.body.volume : citation.volume;
-      citation.pages = req.body.pages ? req.body.pages : citation.pages;
-      citation.type = req.body.type ? req.body.type : citation.type;
-      citation.title = req.body.title ? req.body.title : citation.title;
-      citation.annotation = req.body.annotation ? req.body.annotation : citation.annotation;
-      citation.doi = req.body.doi ? req.body.doi : citation.doi;
-      citation.paper_id = req.body.paper_id ? req.body.paper_id : citation.paper_id;
-
-      citation.save(function (err, citation) {
+    citationModel.findOneAndUpdate(
+      { _id: id },
+      { $pull: { assessments: { rubric_id: req.body.rubric_id } } },
+      { new: true },
+      function (err, val) {
         if (err) {
           return res.status(500).json({
-            message: 'Error when updating citation.',
+            message: 'Could not remove field from array',
             error: err
           });
         }
-
-        return res.json(citation);
+        return res.status(200).json({
+          message: 'Updated array successfully'
+        });
       });
-    });
   },
 
   add_assessment: function (req, res) {
     var id = req.params.id;
+
+    //Find the current citation
     citationModel.findById(id, function (err, citation) {
       if (err) {
         return res.status(500).json({
@@ -158,63 +108,27 @@ module.exports = {
         });
       }
 
-      // Check to see if assessment has already been made for this rubric
-      let assessments = citation.assessments;
-
+      //Get new assessment ready
       let new_assessment = {
         'rubric_id': req.body.rubric_id,
         'rubric_index': req.body.rubric_index,
         'annotation': req.body.annotation
       }
 
-      //Test for empty array
-      if (citation.assessments.length === 0) {
-        console.log(`array was empty, citation: ${citation}`)
-        citation.assessments.push(new_assessment);
-        citation.evaluated = true;
+      //Push new item onto array
+      citation.assessments.push(new_assessment);
+      citation.evaluated = true;
 
-        citation.save(function (err, citation) {
-          if (err) {
-            return res.status(500).json({
-              message: 'Error when updating citation.',
-              error: err
-            });
-          }
-          return res.status(201).json(citation);
-        });
-      } else {
-
-        let check = '';
-
-        //Test to see if rubric has already been used for this citation
-        for (index in assessments) {
-          if (assessments[index].rubric_id.equals(req.body.rubric_id)) {
-            check = 'exists';
-          } else {
-            check = 'does not exist';
-          }
-        }
-        if (check === 'exists') {
-          return res.status(304).json({
-            message: 'Rubric assessment already exists.'
-          });
-        } else {
-
-          citation.assessments.push(new_assessment);
-
-          citation.evaluated = true;
-
-          citation.save(function (err, citation) {
-            if (err) {
-              return res.status(500).json({
-                message: 'Error when updating citation.',
-                error: err
-              });
-            }
-            return res.status(201).json(citation);
+      //Save to DB
+      citation.save(function (err, citation) {
+        if (err) {
+          return res.status(500).json({
+            message: 'Error when updating citation.',
+            error: err
           });
         }
-      }
+        return res.status(201).json(citation);
+      });
     });
   },
 
