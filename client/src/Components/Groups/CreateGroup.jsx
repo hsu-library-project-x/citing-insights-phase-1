311a7481837c9c9d4168e3769cd96bc39cb10547
smittythehippy <fenderpearl72@gmail.com>
2020-06-02 10:12:10 -0700

    added Email Validation

--- client/src/Components/Groups/CreateGroup.jsx
+++ client/src/Components/Groups/CreateGroup.jsx
@@ -24,17 +24,27 @@ class CreateGroup extends Component {
     }
 
     handleValidation = (members) => {
+
         console.log(members);
         //Need to delimit CSV, and validate emails
         const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
+        let valid = true;
+        let invalid_email="";
+        
+        for(let i=0; i < members.length; i++){
+            if(expression.test(String(members[i]).toLowerCase()) === false){
+                valid = false;
+                invalid_email = members[i];
+            }
+        }
 
-        let member_parsed = Papa.parse(members);
-
-        return member_parsed.data;
-
-        //expression.test(String(email).toLowerCase())
-
+        if(valid){
+            return "true";
+        }
+        else{
+            return invalid_email;
+        }
     };
 
     handleOpen = () => {
@@ -82,6 +92,12 @@ class CreateGroup extends Component {
 
                             let member_array = Papa.parse(this.state.Members).data;
 
+                            let validationCheck = this.handleValidation(member_array);
+
+                            if(validationCheck !== "true"){
+                                this.handleAlert('Cannot add emails; one or more are invalid', 'error');
+                            }
+                            else{
                             console.log(member_array);
                             let data = {
                                 creator: this.props.user.email,
@@ -110,7 +126,7 @@ class CreateGroup extends Component {
                                     }
                                     this.handleClose();
                                 })
-                        }}
+                        }}}
                         >
                             <FormControl>
                                 <TextField
